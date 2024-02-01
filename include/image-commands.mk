7527239a1b22e8e439705bcf43528b9b9c2b817e
Daniel Golle <daniel@makrotopia.org>
2024-02-01 05:03:56 +0000

    scripts: ubinize-image.sh: support static volumes, make size optional
    In order to support devices having TF-A FIP image or UBI-aware U-Boot
    SPL we need to include a static volume for the bootloader.
    
    Introduce support for adding additional static volumes by prefixing
    the filename with ':', eg.
    
    UBINIZE_PARTS := fip:=$(STAGING_DIR_IMAGE)/u-boot.fip
    
    Also add support for rootfs-in-uImage.FIT setups which don't require a
    rootfs partition and make the (3rd) size parameter in UBINIZE_PARTS
    optional (see example above without declared size).
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -136,9 +136,11 @@ endef
 define Build/append-ubi
 	sh $(TOPDIR)/scripts/ubinize-image.sh \
 		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
-		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
 		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
-		--rootfs $(IMAGE_ROOTFS) \
+		$(if $(findstring fit,$(1)), \
+		$(if $(KERNEL_IN_UBI),--part fit=$(IMAGE_KERNEL)), \
+		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
+		--rootfs $(IMAGE_ROOTFS)) \
 		$@.tmp \
 		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
 		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \
