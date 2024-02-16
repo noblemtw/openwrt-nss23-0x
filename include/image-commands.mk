e8872807110c9bcb482bca461a9bc977a412de1c
Daniel Golle <daniel@makrotopia.org>
2024-02-16 05:32:51 +0000

    scripts: ubinize-image.sh: fix on POSIX shell, allow custom images
    Make sure ubinize-image.sh also works with more simple POSIX Shell and
    allow creating complete custom images to be used as ARTIFACT/foo.img
    and thereby allow including uImage.FIT, TF-A FIP and what ever else
    is required on a specific board.
    
    Fixes: 6c17d71973 ("scripts: ubinize-image.sh: support static volumes, make size optional")
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -136,11 +136,9 @@ endef
 define Build/append-ubi
 	sh $(TOPDIR)/scripts/ubinize-image.sh \
 		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
-		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
-		$(if $(findstring fit,$(1)), \
-		$(if $(KERNEL_IN_UBI),--part fit=$(IMAGE_KERNEL)), \
 		$(if $(KERNEL_IN_UBI),--kernel $(IMAGE_KERNEL)) \
-		--rootfs $(IMAGE_ROOTFS)) \
+		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
+		--rootfs $(IMAGE_ROOTFS) \
 		$@.tmp \
 		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
 		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \
@@ -148,6 +146,18 @@ define Build/append-ubi
 		$(UBINIZE_OPTS)
 	cat $@.tmp >> $@
 	rm $@.tmp
+endef
+
+define Build/ubinize-image
+	sh $(TOPDIR)/scripts/ubinize-image.sh \
+		$(if $(UBOOTENV_IN_UBI),--uboot-env) \
+		$(foreach part,$(UBINIZE_PARTS),--part $(part)) \
+		--part $(word 1,$(1))="$(BIN_DIR)/$(DEVICE_IMG_PREFIX)-$(word 2,$(1))" \
+		$@ \
+		-p $(BLOCKSIZE:%k=%KiB) -m $(PAGESIZE) \
+		$(if $(SUBPAGESIZE),-s $(SUBPAGESIZE)) \
+		$(if $(VID_HDR_OFFSET),-O $(VID_HDR_OFFSET)) \
+		$(UBINIZE_OPTS)
 endef
 
 define Build/ubinize-kernel
